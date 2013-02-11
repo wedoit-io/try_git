f0f8883fa2e54e1b57c7964d5f8ce3dcb5fb3d62
Stefano Giorgi <s.giorgi@apexnet.it>
2013-02-11 15:36:36 +0100

    New Controller

---
+++ TryGitSample/TryGitSample/NewViewController.m
@@ -0,0 +1,38 @@
+//
+//  NewViewController.m
+//  TryGitSample
+//
+//  Created by Stefano Giorgi on 2/11/13.
+//  Copyright (c) 2013 Apexnet. All rights reserved.
+//
+
+#import "NewViewController.h"
+
+@interface NewViewController ()
+
+@end
+
+@implementation NewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
