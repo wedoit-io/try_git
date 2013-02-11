88ac46abd14076bb888982c438df42e29877527a
Stefano Giorgi <s.giorgi@apexnet.it>
2013-02-11 15:36:36 +0100

    New Controller

---
+++ TryGitSample/TryGitSample/NewViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewViewController.h
+//  TryGitSample
+//
+//  Created by Stefano Giorgi on 2/11/13.
+//  Copyright (c) 2013 Apexnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewViewController : UIViewController
+
+@end
